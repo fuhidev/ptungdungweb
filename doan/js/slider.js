50c0754a258fee7d71f72ece6f4f211807e35524
FUHI <fuhi.dev@gmail.com>
2017-03-26 23:25:29 +0700

    change slider

--- doan/js/slider.js
+++ doan/js/slider.js
@@ -1,30 +1,13 @@
 var slideIndex = 1,
-    check = [12, 14, 19, 23, 27],
-    check =[
-        {
-            id:12,
-            re:/^(<h1>)(\w?)+(<\/h1>)$/
-        },
-        {
-            id:14,
-            re:undefined
-        },
-        {
-            id:19,
-            re:undefined
-        },
-        {
-            id:23,
-            re:undefined
-        },
-        {
-            id:27,
-            re:undefined
-        }
-    ]
+
     imgs = [];
 for (var i = 1; i <= 29; i++) {
-    imgs.push('img/lession/1/' + i + '.jpg');
+    imgs.push(
+        {
+            id:i,
+            re:i==12?/(<h1>)(\w?)+(<\/h1>)/:i==14?/(<p>)(\w?)+(<\/p>)/:i==19?/(<input)[\s\w]*( type="email")([\s\w]*)(>)/:i==23?/(<input)[\s\w]*( type="submit")([\s\w]*)(>)/:i==27?/(<input)[\s\w]*( type="email")([\s\w]*)( placeholder="Your email")([\s\w]*)(>)/:undefined,
+         img:'img/lession/1/' + i + '.jpg'
+        });
 }
 
 function loadImage(images) {
@@ -32,48 +15,52 @@ function loadImage(images) {
 
     images.forEach(function (value, key) {
         var img = document.createElement('img');
-        img.src = value;
+        img.src = value.img;
         img.className = "mySlides";
         content.appendChild(img);
     });
 
 }
 
-function contains(a, obj) {
+function findSlide(obj) {
+    var a = imgs;
     for (var i = 0; i < a.length; i++) {
-        if (a[i] === obj) {
-            return true;
+        if (a[i].id === obj) {
+            return a[i];
         }
     }
-    return false;
+    return undefined;
 }
+
 function plusDivs(n) {
     showDivs(slideIndex += n);
 }
 
-function currentDiv(n) {
-    showDivs(slideIndex = n);
+function currentDiv() {
+    showDivs(slideIndex);
 }
 
 function showDivs(n) {
     var i;
     var x = document.getElementsByClassName("mySlides");
+
     if (n > x.length || n < 1) {
         return;
     }
     if (n < 1) {
         slideIndex = x.length
     }
-    if(contains(check,n)){
-       if(!checkCoder()) document.getElementById('next').style.visibility="hidden";
 
-    }else{
-        document.getElementById('next').style.visibility="";
-    }
     for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
+        x[i].style.display = slideIndex - 1 === i?"block":"none";
     }
-    x[slideIndex - 1].style.display = "block";
     var span = document.getElementById('numslide');
     span.innerText = [slideIndex, '/', imgs.length].join('');
+    var slide = findSlide(n);
+    if (slide != undefined && slide.re != undefined) {
+            document.getElementById('next').style.visibility = checkCoder(slide.re)?"":"hidden";
+
+    }else{
+         document.getElementById('next').style.visibility ="";
+    }
 }
